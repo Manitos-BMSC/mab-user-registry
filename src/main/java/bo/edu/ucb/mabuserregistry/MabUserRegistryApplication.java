37b3682e2a6424e94e4dde23b118635672dd75e3
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-10-05 20:40:30 -0400

    first commit

---
+++ src/main/java/bo/edu/ucb/mabuserregistry/MabUserRegistryApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.mabuserregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabUserRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabUserRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/mabuserregistry/MabUserRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.mabuserregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabUserRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
