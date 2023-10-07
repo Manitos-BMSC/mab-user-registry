4289bdebf2a745bcdc820a35cd73893ef2fb0ec3
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-10-07 09:49:38 -0400

    Configuration with Eureka client done

--- src/main/java/bo/edu/ucb/mabuserregistry/MabUserRegistryApplication.java
+++ src/main/java/bo/edu/ucb/mabuserregistry/MabUserRegistryApplication.java
@@ -2,8 +2,12 @@ package bo.edu.ucb.mabuserregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class MabUserRegistryApplication {
 
 	public static void main(String[] args) {
