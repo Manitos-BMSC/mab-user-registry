3519cadddf400290b636aa0472fefc6d76d8523b
IsaiDiaz <jsoza057@gmail.com>
2023-10-29 00:05:34 -0400

    Security configurations agregated

---
+++ src/main/java/bo/edu/ucb/mabuserregistry/config/GlobalSecurityConfiguration.java
@@ -0,0 +1,42 @@
+package bo.edu.ucb.mabuserregistry.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class GlobalSecurityConfiguration {
+
+    private final KeycloakJwtTokenConverter keycloakJwtTokenConverter;
+
+    public GlobalSecurityConfiguration(TokenConverterProperties properties) {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter
+                = new JwtGrantedAuthoritiesConverter();
+        this.keycloakJwtTokenConverter
+                = new KeycloakJwtTokenConverter(
+                jwtGrantedAuthoritiesConverter,
+                properties);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests( (authorizeHttpRequests) -> {
+            authorizeHttpRequests
+                    .requestMatchers("/api/v1/doctor/*").permitAll()
+                    .requestMatchers("/api/v1/registry/patient").permitAll()
+                    .requestMatchers("api/v1/registry/dcotor").hasRole("doctorJefe")
+                    .anyRequest()
+                    .denyAll();
+        });
+        http.oauth2ResourceServer( (oauth2) -> {
+            oauth2.jwt( (jwt) -> jwt.jwtAuthenticationConverter(keycloakJwtTokenConverter));
+        });
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
