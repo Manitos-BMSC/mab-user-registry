7b2f05131f6402cb533aa5d5d84b3c2aa710fc2e
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-11-06 09:23:24 -0400

    ZIPKIN configured on server

--- src/main/java/bo/edu/ucb/mabuserregistry/api/DoctorApi.java
+++ src/main/java/bo/edu/ucb/mabuserregistry/api/DoctorApi.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/doctor")ttatus
+@RequestMapping(path = "api/v1/doctor")
 public class DoctorApi {
 
     @Autowired
